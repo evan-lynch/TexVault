dda49a98c26ba0a0bde6412fd0f46f8fa0d743dd
Evan Lynch <149636614+evan-lynch@users.noreply.github.com>
2026-03-15 16:09:52 -0400

    clear main

--- mathjax-config.js
+++
@@ -1,9 +0,0 @@
-window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    packages: {'[+]': ['ams']}
-  },
-  svg: { fontCache: 'global', scale: 0.9 },
-  startup: { typeset: false }
-};
